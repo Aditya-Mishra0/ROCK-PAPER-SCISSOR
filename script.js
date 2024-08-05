03c7bfb653b90710146450674a9638080397aea5
Aditya-Mishra0 <Adityaa.mishra1012@gmail.com>
2024-08-05 21:55:22 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,8 @@
+console.log("hello world")
+getComputerChoice(Rock,Paper,Scissor);
+{
+    const randomIndex = Math.floor(Math.random()*3);
+    return choices[randomIndex];
+}
+
+console.log("getComputerChoice")
