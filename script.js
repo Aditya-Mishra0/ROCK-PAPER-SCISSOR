d2c5c048726d2824deefab2b2485938e5cc106fe
Aditya-Mishra0 <Adityaa.mishra1012@gmail.com>
2024-08-05 22:38:12 +0530

    Delete script.js

--- script.js
+++
@@ -1,8 +0,0 @@
-console.log("hello world")
-getComputerChoice(Rock,Paper,Scissor);
-{
-    const randomIndex = Math.floor(Math.random()*3);
-    return choices[randomIndex];
-}
-
-console.log("getComputerChoice")
