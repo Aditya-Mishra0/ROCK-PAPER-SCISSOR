19515aa63b2be471062aa68c63839680ea3f54f1
Aditya-Mishra0 <Adityaa.mishra1012@gmail.com>
2024-08-06 23:20:00 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -4,55 +4,59 @@ const Rock1 = document.querySelector('#Rock');
 const Paper1 = document.querySelector('#Paper');
 const Scissor2 = document.querySelector('#Scissor');
 
-function getComputerChoice() {
-    const Action = Math.floor(Math.random() * 3);
+function getComputerChoice()
+{
+    const Action = Math.floor(Math.random()*3);
 
-    switch(Action) {
+    switch(Action){
         case 0:
-            console.log("Rock");
-            return "Rock";
+        console.log("Rock");
+        break;
         case 1:
             console.log("Scissor");
-            return "Scissor";
-        case 2:
-            console.log("Paper");
-            return "Paper";
-    }
-}
+            break;
+            case 2:
+                console.log("Paper");
+                break;
 
-console.log(getComputerChoice());
+    }
+    return Action;
+}
+console.log(getComputerChoice(3));
+
 
 function getPlayerChoice() {
-    Rock1.addEventListener('click', () => {
-        playGame("Rock");
-    });
 
-    Paper1.addEventListener('click', () => {
-        playGame("Paper");
-    });
+  Rock1.addEventListener('click', () => {
+      playGame("Rock");
+  });
 
-    Scissor2.addEventListener('click', () => {
-        playGame("Scissor");
-    });
+  Paper1.addEventListener('click', () => {
+      playGame("Paper");
+  });
+
+  Scissor2.addEventListener('click', () => {
+      playGame("Scissor");
+  });
 }
+getPlayerChoice();  
 
-getPlayerChoice();
+console.log(getPlayerChoice);
 
-function playGame(playerChoice) {
-    const computerChoice = getComputerChoice();
-    console.log(`Player chose: ${playerChoice}`);
-    console.log(`Computer chose: ${computerChoice}`);
-
-    if (playerChoice === computerChoice) {
-        console.log("It's a tie");
-    } else if (
-        (playerChoice === "Rock" && computerChoice === "Scissor") ||
-        (playerChoice === "Paper" && computerChoice === "Rock") ||
-        (playerChoice === "Scissor" && computerChoice === "Paper")
-    ) {
-        console.log("Player wins");
-    } else {
-        console.log("Computer wins");
-    }
+function Game(){
+  if  (getComputerChoice===getplayerchoice){
+    console.log("It's a tie");
+  }
+  else if  (
+    (getComputerChoice ==="Rock" && getplayerchoice ==="Paper")
+    (getComputerChoice === "Paper" && getplayerchoice === "Scissor")
+    (getComputerChoice === "Scissor" && getplayerchoice === "Rock")
+  ){
+    console.log("Player wins");
+  }
+else
+{
+console.log("Computer wins");
+}
 }
 
